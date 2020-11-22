915e4e85d3bf760de57c73079f03196c58fb6407
1lizard <67966038+1lizard@users.noreply.github.com>
2020-11-22 13:33:23 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,23 +5,19 @@
 makeCacheMatrix <- function(m = matrix()) {
   inverseee <- NULL
   s <- function(y) {
-    original.matrix <<- y
-    inverseee <<- NULL
+   original.matrix <<- y
+   inverseee <<- NULL
   }
-  g <- function() m
-  setinverse <- function(solve) inverseee <<- solve
-  getinverse <- function() inverseee
-  
-  list(
-    s = s, 
-    z = z,
-    setinverse = setinverse,
-    getinverse = getinverse)
-  
+ g <- function() m
+ setinverse <- function(solve) inverseee <<- solve
+ getinverse <- function() inverseee
+ list(
+ s = s, 
+ z = z,
+ setinverse = setinverse,
+ getinverse = getinverse)
 }
-
 ## Calculates the inverse of a matrix
-
 cacheSolve <- function(p, ...) {
   inverseee <- p$getinverse()
   return(inverseee)
